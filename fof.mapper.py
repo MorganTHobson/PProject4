fbdea86098974f368866d4263b170098cb799a21
parallel <parallel@parallel-pr3.win.ad.jhu.edu>
2018-04-13 18:28:56 -0400

    added working mapper

---
+++ fof.mapper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#Python Mapper for FOF Map/Reduce
+import sys
+
+for line in sys.stdin.readlines():
+  tokens = line.split()
+
+  user = tokens[0]
+  friends = sorted(tokens[1:len(tokens)])
+
+  friends_str = ""
+  for friend in friends:
+    friends_str = friends_str + friend + " "
+
+  friends_str = friends_str[:-1]
+
+  for friend in friends:
+    if user <= friend:
+      sys.stdout.write(user + " " + friend + " " + friends_str + "\n")
+    else:
+      sys.stdout.write(friend + " " + user + " " + friends_str + "\n")
---
+++ fof.reducer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#Python Reducer for FOF Map/Reduce
+import sys
+
+for line in sys.stdin.readlines():
+
